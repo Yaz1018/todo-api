9c25f9519ecb8729a4552cd8884858a30930980e
Garon Davis <garoncdavis@gmail.com>
2015-10-28 19:28:32 -0400

    added variable playground

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+/*
+var person = {
+    name: 'Andrew',
+    age: 21
+};
+
+function updatePerson (obj) {
+    obj.age = 24
+}
+
+updatePerson(person);
+console.log(person);
+*/
+
+//array Example
+
+var grades = [15,37];
+
+function addGrade (grade) {
+
+grades.push(grade);
+    debugger;
+}
+
+addGrade(5);
+console.log(grades);
+
+function changeGrade (grade) {
+
+}
