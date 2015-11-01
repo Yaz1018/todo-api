d2dcceb9d9460025e7a7b247172a27e76409553c
Garon Davis <garoncdavis@gmail.com>
2015-11-01 16:59:39 -0500

    final todo api updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=false&q=work
 app.get('/todos', function (req, res) {
         var queryParams = req.query;
         var filteredTodos = todos;
@@ -28,6 +28,16 @@ app.get('/todos', function (req, res) {
         } else if (!_.isEmpty(queryParams)){
             res.status(404).send();
         }
+
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+
+    };
+
+  //'Go to work on Saturday'.indexOf('work')
 
         res.json(filteredTodos);
     })
