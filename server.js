42b0505c687ae2e3bfd064e9a9115fbc2a32b8f2
Garon Davis <garoncdavis@gmail.com>
2015-11-04 15:04:26 -0500

    added token generation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
-var bcrypt = require('bcrypt')
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -155,14 +155,14 @@ app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.authenticate(body).then(function (user) {
-        res.json(user.toPublicJSON());
+        res.header('Auth', user.generateToken('authenticate')).json(user.toPublicJSON());
     }, function () {
         res.status(401).send();
     });
 });
 
 
-db.sequelize.sync({force: true}).then(function () {
+db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log('Express listening on port ' + PORT);
     });
